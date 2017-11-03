321764b443d413edb365db6e6840a2e4872e6b31
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2017-11-03 10:14:02 +0100

    add buld.sh and build-parts.sh

--- build-oasp.sh
+++ build-oasp.sh
@@ -21,17 +21,3 @@ done
 
 # Setup the OASP project as "image-puller" to be used in other projects in the same cluster
 oc policy add-role-to-group system:image-puller system:authenticated --namespace=oasp
-
-# Create project to host Devportal applications built from OASP's base-images
-oc new-project devportal --display-name="Dev Portal" --description="Dev Portal reference application for Devonfw"
-
-# Create Angular templates for Devportal
-oc create -f https://raw.githubusercontent.com/Jorge-Dacal/prueba/master/templates/devportal/devonfw-devportal-angular-template.json --namespace=devportal
-
-# Create Angular application out of the Angular template
-oc new-app --template=devonfw-devportal-angular-sample --namespace=devportal
-
-#sleep 10
-
-# Build Angular application (sets the E.V. in the Angular code)
-oc start-build devportal-angular --namespace=devportal
